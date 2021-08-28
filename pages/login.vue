01cad4b222deb78479f12272fa49c98abfbffbed
kadukimochida <dolce.km.3a@gmail.com>
2021-08-28 20:26:12 +0900

    firstcommit

---
+++ pages/login.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="login">
+    <label>メールアドレス：<input v-model="email" type="email" required /></label>
+    <br />
+    
+  </div>
+</template>
---
+++ pages/register.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="register">
+    <label>メールアドレス：<input v-model="email" type="email" required /></label>
+    <br />
+    <label>パスワード：<input v-model="password" type="password" required /></label>
+    <br/>
+    <NuxtLink to="/">戻る</NuxtLink>
+  </div>
+</template>
+
+<script>
+import firebase from '~/plugins/firebase'
+export default {
+  data(){
+    return {
+      email:null,
+      password:null,
+    }
+  },
+  methods: {
+    register(){
+      if (!this.email || !this.password) {
+        alert('メールアドレスまたはパスワードが入力されていません')
+        return
+      }
+    },
+  },
+}
+</script>
